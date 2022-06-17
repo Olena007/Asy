7d56c6f6152b7b50df10571f26076b57ad2ecdff
lena <llogvinovaaaa@gmail.com>
2022-06-17 23:22:38 +0200

    asy

---
+++ Asy/Asy/Models/Tasks.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asy.Models
+{
+    internal class Tasks
+    {
+        private string _hello;
+        private string _world;
+
+        public async Task<string> Hello() 
+        {
+            using (StreamReader reader = new StreamReader(@"hello.txt"))
+            {
+                this._hello = await reader.ReadToEndAsync();
+            }
+
+            
+            return this._hello;
+        }
+
+        public async Task<string> World()
+        {
+            using (StreamReader reader = new StreamReader(@"world.txt"))
+            {
+                this._world = await reader.ReadToEndAsync();
+            }
+
+            return this._world;
+        }
+
+        public async Task<string> Both()
+        {
+            string res = "";
+
+            await Task.Run( async() =>
+            {
+                await Hello();
+                await World();
+
+                res = _hello + " " + _world;
+
+                Console.WriteLine(res);
+            }
+                );
+            return res;
+        }
+    }
+}
---
+++ Asy/Asy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Asy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Models.Tasks tasks = new Models.Tasks();
+
+            tasks.Both().Wait();
+        }
+    }
+}
